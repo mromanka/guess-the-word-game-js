1c44168a3f92b7c93cb3379e5d3af41f0ef60ebd
magda <mkroman97@gmail.com>
2025-04-11 07:32:55 +1000

    Fetch random words and count remaining guesses

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-// 1. CREATE GLOBAL VARIABLES TO TARGET HTML ELEMENTS
+// 1. GLOBAL VARIABLES TO TARGET HTML ELEMENTS
 
 const guessedLettersElement = document.querySelector(".guessed-letters"); // UNORDERED LIST WHERE THE PLAYER'S GUESSED LETTERS WILL APPEAR
 const guessButton = document.querySelector(".guess"); // BUTTON WITH THE TEXT "GUESS!" IN IT
 const guessLetterInput = document.querySelector(".letter"); // TEXT INPUT WHERE PLAYER WILL GUESS A LETTER
 const wordInProgress = document.querySelector(".word-in-progress"); // EMPTY PARAGRAPH WHERE THE WORD IN PROGRESS WILL APPEAR
-const remainingGuesses = document.querySelector(".remaining"); // PARAGRAPH WHERE THE REMAINING GUESSES WILL DISPLAY
+const remainingGuessesElement = document.querySelector(".remaining"); // PARAGRAPH WHERE THE REMAINING GUESSES WILL DISPLAY
 const remainingGuessesSpan = document.querySelector(".remaining span"); // SPAN INSIDE THE PARAGRAPH WHERE REMAINING GUESSES WILL DISPLAY
 const message = document.querySelector(".message"); // EMPTY PARAGRAPH WHERE MESSAGES WILL APPEAR WHEN THE PLAYER GUESSES A LETTER
 const playAgainButton = document.querySelector(".play-again"); // HIDDEN BUTTON THAT WILL APPEAR PROMPTING PLAYER TO PLAY AGAIN
 
-const word = "magnolia"; // STARTING WORD TO TEST OUT THE GAME
+let word = "magnolia"; // STARTING WORD TO TEST OUT THE GAME
 let guessedLetters = []; // ARRAY FOR GUESSED LETTERS
+let remainingGuesses = 8; // MAXIMUM NUMBER OF GUESSES
 
-// 2. WRITE A FUNCTION TO ADD PLACEHOLDERS FOR EACH LETTER THAT WILL APPEAR IN THE WORD IN PROGRESS ELEMENT (CREATE AN ARRAY, LOOP THROUGH EACH LETTER OF THE WORD AND ADD IT TO THE END OF THE ARRAY AS THE SYMBOL, JOIN ARRAY BACK TO A STRING AND DISPLAY AS WORD IN PROGRESS)
+// 10. ASYNC FUNCTION TO FETCH RANDOM WORDS
+
+const getWord = async function (){
+    const res = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await res.text();
+    console.log(words);
+    const wordArray = words.split("\n");
+    const randomIndex = Math.floor(Math.random() * wordArray.length)
+    word = wordArray[randomIndex].trim();
+    addPlaceholders(word);
+};
+getWord(); // Calling this function starts the game
+
+// 2. FUNCTION TO ADD PLACEHOLDERS FOR EACH LETTER THAT WILL APPEAR IN THE WORD IN PROGRESS ELEMENT (CREATE AN ARRAY, LOOP THROUGH EACH LETTER OF THE WORD AND ADD IT TO THE END OF THE ARRAY AS THE SYMBOL, JOIN ARRAY BACK TO A STRING AND DISPLAY AS WORD IN PROGRESS)
 
 const addPlaceholders = function (word){
     const placeholder = [];
@@ -22,9 +36,8 @@ const addPlaceholders = function (word){
     };
     wordInProgress.innerText = placeholder.join("");
 };
-addPlaceholders(word);
 
-// 3. ADD A CLICK EVENT FOR THE GUESS BUTTON (CAPTURE THE VALUE OF THE INPUT, LOG OUT THE VALUE OF THE INPUT, THEN EMPTY THE VALUE OF THE INPUT)
+// 3. CLICK EVENT FOR THE GUESS BUTTON (CAPTURE THE VALUE OF THE INPUT, LOG OUT THE VALUE OF THE INPUT, THEN EMPTY THE VALUE OF THE INPUT)
 
 guessButton.addEventListener("click", function(e){
     e.preventDefault();
@@ -39,7 +52,7 @@ guessButton.addEventListener("click", function(e){
     guessLetterInput.value = "";
 });
 
-// 4. WRITE A FUNCTION TO CHECK PLAYER'S INPUT, THEN VALIDATE THE INPUT IN THE GUESS BUTTON EVENT HANDLER
+// 4. FUNCTION TO CHECK PLAYER'S INPUT, THEN VALIDATE THE INPUT IN THE GUESS BUTTON EVENT HANDLER
 
 const validateInput = function(input){
     const acceptedLetter = /[a-zA-Z]/;
@@ -54,7 +67,7 @@ const validateInput = function(input){
     };
 };
 
-// 5. WRITE A FUNCTION TO CAPTURE INPUT AND ADD TO GUESSED LETTERS ARRAY
+// 5. FUNCTION TO CAPTURE INPUT AND ADD TO GUESSED LETTERS ARRAY
 
 const makeGuess = function(guess){
     guess = guess.toUpperCase();
@@ -64,11 +77,12 @@ const makeGuess = function(guess){
         guessedLetters.push(guess);
         console.log(guessedLetters);
         displayGuessedLetters();
+        countGuessesRemaining(guess);
         updateWordInProgress(guessedLetters);
     };
 };
 
-// 6. WRITE A FUNCTION TO SHOW THE GUESSED LETTERS (LOOP THROUGH EACH LETTER OF THE guessedLetters ARRAY, CREATE A NEW LIST ITEM FOR EACH LETTER, ADD NEW LIST ITEM TO THE guessedLettersElement)
+// 6. FUNCTION TO SHOW THE GUESSED LETTERS (LOOP THROUGH EACH LETTER OF THE guessedLetters ARRAY, CREATE A NEW LIST ITEM FOR EACH LETTER, ADD NEW LIST ITEM TO THE guessedLettersElement)
 
 const displayGuessedLetters = function(){
     guessedLettersElement.innerHTML = "";
@@ -79,7 +93,7 @@ const displayGuessedLetters = function(){
     };
 };
 
-// 7. WRITE A FUNCTION TO UPDATE THE WORD IN PROGRESS WITH THE CORRECTLY GUESSED LETTERS TO REPLACE THE PLACEHOLDER SYMBOLS
+// 7. FUNCTION TO UPDATE THE WORD IN PROGRESS WITH THE CORRECTLY GUESSED LETTERS TO REPLACE THE PLACEHOLDER SYMBOLS
 
 const updateWordInProgress = function(guessedLetters){
     const wordUpper = word.toUpperCase();
@@ -98,7 +112,26 @@ const updateWordInProgress = function(guessedLetters){
     checkIfPlayerWon();
 };
 
-// 8. WRITE A FUNCTION TO CHECK IF THE PLAYER WON
+// 9. FUNCTION TO COUNT GUESSES REMAINING
+
+const countGuessesRemaining = function (guess){
+    const wordUpper = word.toUpperCase();
+    if (!wordUpper.includes(guess)) {
+        message.innerText = `The word doesn't contain the letter ${guess}`;
+        remainingGuesses -= 1;
+    } else {
+        message.innerText = `The word contains the letter ${guess}`;
+    };
+    if (remainingGuesses === 0){
+        message.innerHTML = `Game over! The word is <span class ="highlight">${word}`;
+    } else if (remainingGuesses === 1){
+        remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    }
+}
+
+// 8. FUNCTION TO CHECK IF THE PLAYER WON
 
 const checkIfPlayerWon = function(){
     if (word.toUpperCase() === wordInProgress.innerText){
